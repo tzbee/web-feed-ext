81c9523761930392c3e15aacaf750266f90307ed
Amine Tolba <tolbimine@gmail.com>
2019-03-23 20:17:40 +0200

    UI tweaking

--- src/popup/components/Tabs/TabPanel.js
+++ src/popup/components/Tabs/TabPanel.js
@@ -12,30 +12,46 @@ const DEFAULT_CLASS_MAP = {
 
 const Tab = ({ active, children, classNames = '' }) => {
     const activeClass = active ? 'Tab-active' : 'Tab-inactive';
-    return <div className={`Tab ${activeClass} ${classNames}`}>
-                {children}
-            </div>;
+    return <div className={`Tab ${activeClass} ${classNames}`}>{children}</div>;
 };
 
-export default ({ tabs, selectedTabID, children, selectTabID, classMap = DEFAULT_CLASS_MAP }) => {
+export default ({
+    tabs,
+    selectedTabID,
+    children,
+    selectTabID,
+    classMap = DEFAULT_CLASS_MAP
+}) => {
     const renderChildren = (classNames = '') => {
         return React.Children.map(children, (child, i) => {
             const { id, data } = tabs[i];
-            return <Tab active={id===selectedTabID} classNames={classNames}>
-                {React.cloneElement(child,{...data})}
-            </Tab>;
+            return (
+                <Tab active={id === selectedTabID} classNames={classNames}>
+                    {React.cloneElement(child, { ...data })}
+                </Tab>
+            );
         });
     };
 
     // Only the tabs with a title are navigable
-    const navigableTabs = tabs.filter(tab => tab.title);
+    const navigableTabs = tabs.filter(tab => 'title' in tab);
 
-    const { main: mainClasses='', tabNav, tabNavItem='', tab: tabClasses='' } = classMap;
+    const {
+        main: mainClasses = '',
+        tabNav,
+        tabNavItem = '',
+        tab: tabClasses = ''
+    } = classMap;
 
-    return <div className={`TabPanel ${mainClasses}`}>
-                <TabNav tabs={navigableTabs} selectedTabID={selectedTabID} selectTab={selectTabID} classMap={{tabNav,tabNavItem}}/>
-                <div className='TabPanel-content'>
-                    {renderChildren(tabClasses)}
-                </div>
-            </div>;
+    return (
+        <div className={`TabPanel ${mainClasses}`}>
+            <TabNav
+                tabs={navigableTabs}
+                selectedTabID={selectedTabID}
+                selectTab={selectTabID}
+                classMap={{ tabNav, tabNavItem }}
+            />
+            <div className="TabPanel-content">{renderChildren(tabClasses)}</div>
+        </div>
+    );
 };
--- src/popup/reducers/appReducer.js
+++ src/popup/reducers/appReducer.js
@@ -1,21 +1,21 @@
-const DEFAULT_MAIN_TAB = "FEED_LIST";
-const DEFAULT_FEED_EDITOR_TAB = "GENERAL";
+const DEFAULT_MAIN_TAB = 'FEED_LIST';
+const DEFAULT_FEED_EDITOR_TAB = 'GENERAL';
 
 // The state of the current feed editor
 const DEFAULT_EDITED_FEED = {
     tab: DEFAULT_FEED_EDITOR_TAB,
-    feedID: "",
+    feedID: '',
     data: {
-        title: "",
-        description: "",
-        commandID: "",
+        title: '',
+        description: '',
+        commandID: '',
         args: [],
         query: {}
     }
 };
 
 const DEFAULT_FEED_ITEM_LIST = {
-    feedID: "",
+    feedID: '',
     items: {}
 };
 
@@ -34,25 +34,25 @@ export default (
     { type, tab, commands, feed, feedID, feeds, itemFolding }
 ) => {
     switch (type) {
-        case "SET_EDITED_FEED":
+        case 'SET_EDITED_FEED':
             return Object.assign({}, state, {
                 feedEditor: Object.assign({}, state.feedEditor, { data: feed })
             });
-        case "SET_COMMANDS":
+        case 'SET_COMMANDS':
             return Object.assign({}, state, { commands });
-        case "SET_TAB":
+        case 'SET_TAB':
             return Object.assign({}, state, { tab });
-        case "SET_FEED_LIST":
+        case 'SET_FEED_LIST':
             return Object.assign({}, state, { feeds });
-        case "SET_FEED_ITEM_LIST":
+        case 'SET_FEED_ITEM_LIST':
             return Object.assign({}, state, {
                 feedItemList: Object.assign({}, state.feedItemList, { feedID })
             });
-        case "SET_FEED_EDITOR_TAB":
+        case 'SET_FEED_EDITOR_TAB':
             return Object.assign({}, state, {
                 feedEditor: Object.assign({}, state.feedEditor, { tab })
             });
-        case "LOAD_FEED_TO_EDITOR":
+        case 'LOAD_FEED_TO_EDITOR':
             const { feedEditor, feeds: currentFeeds } = state;
             const feedEditorCopy = Object.assign({}, feedEditor);
             const loadedFeed = currentFeeds.find(feed => feed.id === feedID);
@@ -61,11 +61,11 @@ export default (
             feedEditorCopy.data = loadedFeed;
 
             return Object.assign({}, state, { feedEditor: feedEditorCopy });
-        case "RESET_FEED_EDITOR":
+        case 'RESET_FEED_EDITOR':
             return Object.assign({}, state, {
                 feedEditor: DEFAULT_EDITED_FEED
             });
-        case "TOGGLE_ITEM_FOLDING":
+        case 'TOGGLE_ITEM_FOLDING':
             const { itemID, folding } = itemFolding;
             const feedItemList = Object.assign({}, state.feedItemList);
             const { items } = feedItemList;
