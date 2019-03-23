7792c2ad2a29dee621ba50c98e6c8d1773dcbf1b
Amine Tolba <tolbimine@gmail.com>
2019-03-23 16:17:36 +0200

    UI change, add line separator to feed editor header

--- src/popup/components/Tabs/FeedEditorTabPanel.js
+++ src/popup/components/Tabs/FeedEditorTabPanel.js
@@ -1,3 +1,8 @@
+/*
+    UNUSED
+    TO DELETE
+*/
+
 import * as React from 'react';
 import TabPanel from './TabPanel';
 
@@ -5,36 +10,57 @@ import FeedEditorGeneral from '../FeedEditor/FeedEditorGeneral';
 import FeedEditorCommand from '../FeedEditor/FeedEditorCommand';
 import FeedEditorAdvanced from '../FeedEditor/FeedEditorAdvanced';
 
-export default ({ feed, plugins, commandIDs, selectTabID, selectedTabID, saveFeed, setEditedFeed }) => {
-
+export default ({
+    feed,
+    plugins,
+    commandIDs,
+    selectTabID,
+    selectedTabID,
+    saveFeed,
+    setEditedFeed
+}) => {
     const handleSaveFeed = () => {
         saveFeed && saveFeed(feed);
     };
 
-    const tabs = [{
-        id: 'GENERAL',
-        title: 'General',
-        data: { setEditedFeed }
-    }, {
-        id: 'COMMAND',
-        title: 'Command',
-        data: { setEditedFeed }
-    }, {
-        id: 'ADVANCED',
-        title: 'Advanced',
-        data: { setEditedFeed }
-    }];
+    const tabs = [
+        {
+            id: 'GENERAL',
+            title: 'General',
+            data: { setEditedFeed }
+        },
+        {
+            id: 'COMMAND',
+            title: 'Command',
+            data: { setEditedFeed }
+        },
+        {
+            id: 'ADVANCED',
+            title: 'Advanced',
+            data: { setEditedFeed }
+        }
+    ];
 
     const pluginIDs = plugins.map(plugin => plugin.id);
 
-    return <div className='FeedEditorPanel'>
-                <form action='#' onSubmit={handleSaveFeed}>
-                    <TabPanel tabs={tabs} selectedTabID={selectedTabID} selectTabID={selectTabID}>
-                        <FeedEditorGeneral feed={feed}/>
-                        <FeedEditorCommand feed={feed} pluginIDs={pluginIDs} commandIDs={commandIDs}/>
-                        <FeedEditorAdvanced feed={feed}/>
-                    </TabPanel>
-                    <button>Save</button>
-                </form>
-            </div>;
+    return (
+        <div className="FeedEditorPanel">
+            <form action="#" onSubmit={handleSaveFeed}>
+                <TabPanel
+                    tabs={tabs}
+                    selectedTabID={selectedTabID}
+                    selectTabID={selectTabID}
+                >
+                    <FeedEditorGeneral feed={feed} />
+                    <FeedEditorCommand
+                        feed={feed}
+                        pluginIDs={pluginIDs}
+                        commandIDs={commandIDs}
+                    />
+                    <FeedEditorAdvanced feed={feed} />
+                </TabPanel>
+                <button>Save</button>
+            </form>
+        </div>
+    );
 };
